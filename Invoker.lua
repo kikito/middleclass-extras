979c2c3aacb09596f412f9e911bcc108e019834f
Enrique García <kikito@gmail.com>
2010-10-16 19:04:39 +0200

    renamed Senter.send to Invoker.invoke

--- Invoker.lua
+++ Invoker.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------
--- Sender.lua
+-- Invoker.lua
 -- Enrique García ( enrique.garcia.cota [AT] gmail [DOT] com ) - 4 Mar 2010
 -- Helper function that simplifies method invocation via method names or functions
 -----------------------------------------------------------------------------------
@@ -7,32 +7,32 @@
 --[[ Usage:
 
   require 'MiddleClass' -- or similar
-  require 'middleclass-extras.Sender' -- or 'middleclass-extras.init'
+  require 'middleclass-extras.Invoker' -- or 'middleclass-extras.init'
 
   MyClass = class('MyClass')
-  MyClass:includes(Sender)
+  MyClass:includes(Invoker)
   function MyClass:foo(x,y) print('foo executed with params', x, y) end
 
   local obj = MyClass:new()
 
-  obj:send('foo', 1,2) -- foo executed with params 1 2
-  obj:send( function(self, x, y)
+  obj:invoke('foo', 1,2) -- foo executed with params 1 2
+  obj:invoke( function(self, x, y)
     print('nameless function executed with params', x, y)
   , 3, 4) -- nameless function executed with params 3, 4
   
   Notes:
    * The function first parameter must allways be self
-   * You can use Sender independently: Sender.send(obj, 'method')
+   * You can use Invoker independently: Invoker.invoke(obj, 'method')
 ]]
 
 assert(Object~=nil and class~=nil, 'MiddleClass not detected. Please require it before using Beholder')
 
-Sender = {
+Invoker = {
 
-  send = function(self, methodOrName, ...)
+  invoke = function(self, methodOrName, ...)
     local method = methodOrName
     if(type(methodOrName)=='string') then method = self[methodOrName] end
-    assert(type(method)=='function', 'Sender:send requires a function or function name')
+    assert(type(method)=='function', 'Invoker:invoke requires a function or function name')
     return method(self, ...)
   end
 
--- spec/Invoker_spec.lua
+++ spec/Invoker_spec.lua
@@ -1,21 +1,21 @@
 require('middleclass-extras.init')
 
-context( 'Sender', function()
+context( 'Invoker', function()
 
   local MyClass = class('MyClass')
-  MyClass:include(Sender)
+  MyClass:include(Invoker)
   function MyClass:foo(x,y) return 'foo ' .. tostring(x) .. ', ' .. tostring(y) end
   function MyClass:testSelf() return instanceOf(MyClass, self) end
   
   local obj = MyClass:new()
   
   test('It should work with method names', function()
-    assert_equal(obj:send('foo', 1, 2), 'foo 1, 2')
+    assert_equal(obj:invoke('foo', 1, 2), 'foo 1, 2')
   end)
   
   test('It should work with implicit functions', function()
     assert_equal(
-      obj:send(
+      obj:invoke(
         function(self, x, y) return 'bar '.. tostring(x) .. ', ' .. tostring(y) end,
         3,
         4
@@ -25,8 +25,8 @@ context( 'Sender', function()
   end)
   
   test('It should use self as implicit parameter in all cases', function()
-    assert_true(obj:send('testSelf'))
-    assert_true(obj:send(MyClass.testSelf))
+    assert_true(obj:invoke('testSelf'))
+    assert_true(obj:invoke(MyClass.testSelf))
   end)
 
 end)
