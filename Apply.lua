70f939b3a1fa710db1f2b82aa76845240473c6d4
Enrique García <kikito@gmail.com>
2010-10-20 01:45:54 +0200

    Apply now has both apply and applySorted (Some tests still pending). Updated middleclass submodule.

--- Apply.lua
+++ Apply.lua
@@ -6,24 +6,63 @@
 
 assert(Object~=nil and class~=nil, 'MiddleClass not detected. Please require it before using Apply')
 assert(Invoker~=nil, 'The Apply module requires the Invoker module in order to work. Please require Invoker before requiring Apply')
-assert(Callbacks~=nil, 'The Apply module requires the Callbacks module in order to work. Please require Invoker before requiring Apply')
+
+--[[ Usage:
+
+  require 'MiddleClass' -- or similar
+  require 'middleclass-extras.Apply' -- or 'middleclass-extras.init'
+
+  MyClass = class('MyClass')
+  MyClass:includes(Apply)
+  
+  function MyClass:initialize()
+    super.initialize(self)
+    self.counter = 0
+  end
+  
+  function MyClass:destroy()
+     -- Remove from the list immediately, instead of waiting for the gc
+    self:removeFromApply()
+    super.destroy(self)
+  end
+  
+  -- If you use object:destroyWithCallbacks() instead of object:destroy() removeFromApply will be used automatically
+  
+  function MyClass:count() self.counter = self.counter + 1  end
+  
+  local obj1 = MyClass:new()
+  
+  MyClass:apply('count')
+  
+  local obj2 = MyClass:new()
+  
+  MyClass:apply('count')
+  
+  print(obj1, obj2) -- prints 2   1
+  
+  
+]]
+
 
 -- Private stuff
+
+
+-- The list of instances
 _instances = {}
 
 -- Adds an instance to the "list of instances" of its class
-local function _registerInstance(theClass, instance)
+local function _add(theClass, instance)
   if not includes(Apply, theClass) then return end
   _instances[theClass] = _instances[theClass] or _G.setmetatable({}, {__mode = "k"})
   _instances[theClass][instance] = instance
-  _registerInstance(theClass.superclass, instance)
+  _add(theClass.superclass, instance)
 end
 
--- Removes an instance from the "list of instances" of its class
-local function _unregisterInstance(theClass, instance)
+-- Removes from the "list of instances"
+local function _remove(theClass, instance)
   if not includes(Apply, theClass) then return end
-  _unregisterInstance(theClass.superclass, instance)
-  _instances[theClass][instance] = nil
+  _remove(theClass.superclass, instance)
+  if _instances[theClass] ~= nil then _instances[theClass][instance] = nil end
 end
 
 -- The Apply module
@@ -33,21 +72,40 @@ function Apply:included(theClass)
   if not includes(Callbacks, theClass) then
     theClass:include(Callbacks)
   end
-  theClass:addCallback('after', 'initialize', function(instance)
-    _registerInstance(instance.class, instance)
-  end)
-  theClass:addCallback('after', 'destroy', function(instance)
-    _unregisterInstance(instance.class, instance)
-  end)
+  theClass:addCallback('after', 'initialize', function(instance) _add(instance.class, instance) end)
+  theClass:addCallback('after', 'destroy', 'removeFromApply')
 end
 
--- Applies some method to all the instances of this class (not subclasses)
+function Apply:removeFromApply()
+  _remove(self.class, self)
+end
+
+-- Applies some method to all the instances of this class, including subclasses
 function Apply.apply(theClass, methodOrName, ...)
-  for _,instance in pairs(_instances[theClass]) do
-    if(Invoker.invoke(instance, methodOrName, ...) == false) then return end
-  end
+  return Apply.applySorted(theClass, nil, methodOrName, ... )
 end
 
+-- Applies some method to all the instances of this class, including subclasses
+-- Notes:
+--   * sortFunc can be provided as a meaning of sorting (table.sort will be used)
+--   * a copy of the instances table is allways made so it is safe to call removeFromApply
+function Apply.applySorted(theClass, sortFunc, methodOrName, ...)
+
+  local copy,i = {},1
+  for _,instance in pairs(_instances[theClass]) do
+    copy[i] = instance
+    i = i + 1
+  end
+
+  if type(sortFunc)=='function' then
+    table.sort(copy, sortFunc)
+  end
+
+  for _,instance in ipairs(copy) do
+    if Invoker.invoke(instance, methodOrName, ...) == false then return false end
+  end
+  return true
+end
 
 
 
--- spec/Apply_spec.lua
+++ spec/Apply_spec.lua
@@ -5,6 +5,7 @@ context( 'Apply', function()
   context('When included by a class', function()
   
     local MyClass = class('MyClass')
+    local list = {}
     
     function MyClass:initialize()
       super.initialize(self)
@@ -13,6 +14,14 @@ context( 'Apply', function()
     
     function MyClass:count(increment)
       self.counter = self.counter + increment
+    end
+
+    function MyClass:addToList()
+      table.insert(list, self)
+    end
+    
+    function MyClass:__tostring()
+      return 'MyClass(' .. tostring(self.counter) .. ')'
     end
    
     MyClass:include(Apply)
@@ -29,13 +38,48 @@ context( 'Apply', function()
         MyClass:apply('count', 1)
         assert_equal(obj1.counter, 1)
         assert_equal(obj2.counter, 1)
+        local obj3 = MyClass:new()
+        MyClass:apply('count', 1)
+        assert_equal(obj1.counter, 2)
+        assert_equal(obj3.counter, 1)
+        obj3:destroyWithCallbacks()
       end)
       test('It should work with a function', function()
         MyClass:apply(function(obj, c) obj:count(c) end, 1)
         assert_equal(obj1.counter, 1)
         assert_equal(obj2.counter, 1)
       end)
+      test('It should allow removing objects from inside the call', function()
+        --TODO
+      end)
+    end)
     
+    context('When invoking applySorted', function()
+      test('It should sort the instances properly before executing', function()
+        obj1.counter = 1
+        obj2.counter = 2
+        list = {}
+        MyClass:applySorted(function(a,b) return a.counter < b.counter end, 'addToList')
+        assert_equal(list[1], obj1)
+        assert_equal(list[2], obj2)
+        list = {}
+        MyClass:applySorted(function(a,b) return a.counter > b.counter end, 'addToList')
+        assert_equal(list[1], obj2)
+        assert_equal(list[2], obj1)
+      end)
+    end)
+    
+    context('When destroying elements', function()
+      test('DestroyWithCallbacks should remove instances from the list of objects', function()
+        --TODO
+      end)
+      test('Explicit call of removeFromApply should also work', function()
+        --TODO
+      end)
+    end)
+    
+    context('When subclassing', function()
+      --TODO
     end)
   
   end)
