a92369142ae4bf426ae97fb6758300413e495556
Linus Sjögren <thelinx@unreliablepollution.net>
2010-11-23 20:06:42 +0100

    nice one, self

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local modules = {
   'Invoker', 'GetterSetter', 'Branchy', 'Callbacks', 'Apply', 'Beholder', 'Stateful'
 }
 
-local basename = ({...})[1]:gsub("init", "")
+local basename = ({...})[1]:gsub("%.init", "")
 for _,module in ipairs(modules) do
   require(basename..'.' .. module)
 end
