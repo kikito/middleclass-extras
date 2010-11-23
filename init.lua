f9d58196bc579f153769a62574218c09a375302f
Linus Sjögren <thelinx@unreliablepollution.net>
2010-11-23 20:05:00 +0100

    fixed an issue where init.lua would cause headaches if middleclass-extras wasn't in the löve root

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ local modules = {
   'Invoker', 'GetterSetter', 'Branchy', 'Callbacks', 'Apply', 'Beholder', 'Stateful'
 }
 
+local basename = ({...})[1]:gsub("init", "")
 for _,module in ipairs(modules) do
-  require('middleclass-extras.' .. module)
+  require(basename..'.' .. module)
 end
