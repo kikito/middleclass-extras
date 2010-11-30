75f843a7c90938d8ec6b5ba8b068601083e12604
Linus Sjögren <thelinx@unreliablepollution.net>
2010-11-30 01:19:27 +0100

    Updated init.lua. Fixes #1

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-local modules = {
+local _path = ({...})[1]:gsub("%.init", "")
+local _modules = {
   'Invoker', 'GetterSetter', 'Branchy', 'Callbacks', 'Apply', 'Beholder', 'Stateful'
 }
 
-for _,module in ipairs(modules) do
-  require('middleclass-extras.' .. module)
+for _,module in ipairs(_modules) do
+  require(_path .. '.' .. module)
 end
