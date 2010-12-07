f6a65d0600571e8d95c137b261fbfd62cc446b49
Enrique García <kikito@gmail.com>
2010-12-08 00:25:45 +0100

    updated README. Reached tag 0.7

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- middleclass-extras.lua - v0.7
+-- Enrique García ( enrique.garcia.cota [AT] gmail [DOT] com ) - 19 Oct 2009
+-- Complementary lib for middleclass
+-----------------------------------------------------------------------------------------------------------------------
+
 local _path = ({...})[1]:gsub("%.init", "")
 local _modules = {
   'Invoker', 'GetterSetter', 'Branchy', 'Callbacks', 'Apply', 'Beholder', 'Stateful', 'Indexable'
